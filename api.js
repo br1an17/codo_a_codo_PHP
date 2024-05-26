a8dcc74f646fc41b66a82e9eea75ac10ccce0406
br1an17 <106201381+br1an17@users.noreply.github.com>
2024-05-26 19:57:38 -0300

    prueva de boton para seguir trallendo pelis

--- api.js
+++ api.js
@@ -1,41 +1,40 @@
 const btn = document.getElementById("btn");
-let suma = 0;
+let suma = 1;
 
-const url = "https://api.themoviedb.org/3/movie/popular"; // Cambiamos la URL para obtener películas populares
+const url = `https://api.themoviedb.org/3/movie/popular?page=${suma}`; // Cambiamos la URL para obtener películas populares
 const options = {
   method: "GET",
   headers: {
     accept: "application/json",
     Authorization:
-    "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkOGRjZGRjZDEzYjU4MjA3MzE3ODk5Y2YxN2RjMDYwZiIsInN1YiI6IjY2NTI4YjIxODRiNzQ5YjUzZGY3Y2I0YiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.pcUfWxnJpdFDjvzFIRE_OTuNi2-reKUU7lTavs7sCJU",
-  },
-};
-const traerPelis = ()=>{
-  fetch(url, options)
-  .then((res) => res.json())
-  .then((json) => {
-    const moviesContainer = document.getElementById("pelis");
-    moviesContainer.innerHTML = "";
-    
-    json.results.forEach((movie) => {
-      const movieContent = `
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkOGRjZGRjZDEzYjU4MjA3MzE3ODk5Y2YxN2RjMDYwZiIsInN1YiI6IjY2NTI4YjIxODRiNzQ5YjUzZGY3Y2I0YiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.pcUfWxnJpdFDjvzFIRE_OTuNi2-reKUU7lTavs7sCJU",
+    },
+  };
+  const traerPelis = () => {
+    suma = suma + 1;
+    fetch(url, options)
+    .then((res) => res.json())
+    .then((json) => {
+      const moviesContainer = document.getElementById("pelis");
+      moviesContainer.innerHTML = "";
+      json.results.forEach((movie) => {
+        const movieContent = `
       <div class="peliculas">
-                <a href="./info.html">
-                    <div class="pelicula">
+      <a href="./info.html">
+      <div class="pelicula">
       <img class="imgTendencia" src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title}">
       <div class="tituloPelicula">
       <h4>${movie.title}</h4>
-  </div>
+      </div>
       </div>
       </div>
       <section>
       `;
-      
-      // Insertamos el contenido de la película en el contenedor
-      moviesContainer.innerHTML += movieContent;
-    });
-  })
-  .catch((err) => console.error("error:" + err));
-  
-}
+
+        // Insertamos el contenido de la película en el contenedor
+        moviesContainer.innerHTML += movieContent;
+      });
+    })
+    .catch((err) => console.error("error:" + err));
+};
 btn.addEventListener("click", traerPelis);
