fa755bda7fd7016f07af8bf35a03a75a31619760
br1an17 <106201381+br1an17@users.noreply.github.com>
2024-05-26 22:04:55 -0300

    hacemos el paginado para que pase de pagina mayor a menor

---
+++ paginaPosterior.js
@@ -0,0 +1,56 @@
+const btnPost = document.getElementById("btnPost");
+const btnAnt = document.getElementById("btnAnt");
+let suma = 0;
+
+const options = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization: "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkOGRjZGRjZDEzYjU4MjA3MzE3ODk5Y2YxN2RjMDYwZiIsInN1YiI6IjY2NTI4YjIxODRiNzQ5YjUzZGY3Y2I0YiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.pcUfWxnJpdFDjvzFIRE_OTuNi2-reKUU7lTavs7sCJU",
+  },
+};
+
+const actualizarPelis = () => {
+  const url = `https://api.themoviedb.org/3/movie/popular?page=${suma}`;
+  console.log(suma);
+  
+  fetch(url, options)
+    .then((res) => res.json())
+    .then((json) => {
+      const moviesContainer = document.getElementById("pelis");
+      moviesContainer.innerHTML = "";
+      json.results.forEach((movie) => {
+        const movieContent = `
+        <div class="peliculas">
+          <a href="./info.html">
+            <div class="pelicula">
+              <img class="imgTendencia" src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title}">
+              <div class="tituloPelicula">
+                <h4>${movie.title}</h4>
+              </div>
+            </div>
+          </a>
+        </div>
+        <section>
+        `;
+        
+        moviesContainer.innerHTML += movieContent;
+      });
+    })
+    .catch((err) => console.error("error:" + err));
+};
+
+const traerPelis = () => {
+  suma += 1; // Incrementa `suma` antes de llamar a la API
+  actualizarPelis();
+};
+
+const peliAnterior = () => {
+  if (suma > 1) { // Verifica que suma no sea menor que 1
+    suma -= 1; // Decrementa `suma` antes de llamar a la API
+    actualizarPelis();
+  }
+};
+
+btnPost.addEventListener("click", traerPelis);
+btnAnt.addEventListener("click", peliAnterior);
